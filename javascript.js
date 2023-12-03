ecbabba033878575156196473b902e5b2ee33979
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2023-12-03 22:35:08 +0100

    Added functions for changing diagrams

--- javascript.js
+++ javascript.js
@@ -13,26 +13,76 @@ const submitBtn = document.getElementById("submit-btn").addEventListener("click"
 });
 
 // Analytics-container
+// This code is responsible for creating the diagram inside the analytics-container
+
 const { csv, select, scaleLinear, extent, axisLeft, axisBottom } = d3;
 const container = select('.diagram-container');
 const width = container.node().clientWidth;
 const height = container.node().clientHeight;
+document.getElementById('dm-year-picker').addEventListener("change", changeDataSet);
+function changeDataSet (){
+    const selectedYear = document.getElementById('dm-year-picker').value;
+    console.log(selectedYear);
+    switch(selectedYear){
+        case "2021-data-set":
+
+            salesChartData = [
+                { Month: "Jan", Sales: 26 },
+                { Month: "Feb", Sales: 43 },
+                { Month: "Mar", Sales: 33 },
+                { Month: "Apr", Sales: 18 },
+                { Month: "May", Sales: 22 },
+                { Month: "Jun", Sales: 53 },
+                { Month: "Jul", Sales: 45 },
+                { Month: "Aug", Sales: 65 },
+                { Month: "Sep", Sales: 51 },
+                { Month: "Oct", Sales: 48 },
+                { Month: "Nov", Sales: 76 },
+                { Month: "Dec", Sales: 98 }
+            ];
+            main();
+        break;
+        case "2022-data-set":
+            salesChartData = [
+                { Month: "Jan", Sales: 126 },
+                { Month: "Feb", Sales: 143 },
+                { Month: "Mar", Sales: 110 },
+                { Month: "Apr", Sales: 98 },
+                { Month: "May", Sales: 164 },
+                { Month: "Jun", Sales: 156 },
+                { Month: "Jul", Sales: 190 },
+                { Month: "Aug", Sales: 118 },
+                { Month: "Sep", Sales: 123 },
+                { Month: "Oct", Sales: 142 },
+                { Month: "Nov", Sales: 122 },
+                { Month: "Dec", Sales: 110 }
+            ];
+            main();
+        break;
+        case "2023-data-set":
+            salesChartData = [
+                { Month: "Jan", Sales: 145 },
+                { Month: "Feb", Sales: 134 },
+                { Month: "Mar", Sales: 110 },
+                { Month: "Apr", Sales: 128 },
+                { Month: "May", Sales: 46 },
+                { Month: "Jun", Sales: 156 },
+                { Month: "Jul", Sales: 190 },
+                { Month: "Aug", Sales: 128 },
+                { Month: "Sep", Sales: 93 },
+                { Month: "Oct", Sales: 142 },
+                { Month: "Nov", Sales: 102 },
+                { Month: "Dec", Sales: 170 }
+            ];
+            main();
+        break;
+    }
+}
+
 
 // Data set
-const salesChartData = [
-    { Month: "Jan", Sales: 126 },
-    { Month: "Feb", Sales: 143 },
-    { Month: "Mar", Sales: 110 },
-    { Month: "Apr", Sales: 98 },
-    { Month: "May", Sales: 164 },
-    { Month: "Jun", Sales: 156 },
-    { Month: "Jul", Sales: 190 },
-    { Month: "Aug", Sales: 118 },
-    { Month: "Sep", Sales: 123 },
-    { Month: "Oct", Sales: 142 },
-    { Month: "Nov", Sales: 122 },
-    { Month: "Dec", Sales: 110 }
-];
+var salesChartData = [];
+
 const xValue = (data)=> data.Month;
 const yValue = (data)=> data.Sales;
 const margin = {
@@ -66,6 +116,9 @@ const main = () => {
         p_y: yValue(d),
     }));
 
+    svg.selectAll('.tick line').remove();
+
+    // Append circles
     svg.selectAll('circle')
         .data(mark)
         .join('circle')
@@ -76,7 +129,7 @@ const main = () => {
         .append('title')
         .text(d => `(${d.p_x}, ${d.p_y})`);
 
-        // Append labels
+    // Append labels
     svg.selectAll('text')
         .data(mark)
         .join('text')
@@ -91,6 +144,8 @@ const main = () => {
     const lineGenerator = d3.line()
         .x(d => d.x)
         .y(d => d.y);
+
+    svg.selectAll('path').remove();
 
     svg.append('path')
         .datum(mark)
@@ -134,6 +189,89 @@ const main = () => {
 
     // Remove y-axis line
     yAxis.select('.domain').remove();
+
+    const updateQSales = (salesChartData) => {
+        const q1 = document.getElementById('q1-total-sales');
+        const q2 = document.getElementById('q2-total-sales');
+        const q3 = document.getElementById('q3-total-sales');
+        const q4 = document.getElementById('q4-total-sales');
+
+        // Assuming your data has a structure like:
+        // { Month: "Jan", Sales: 126 }
+        const salesByMonth = salesChartData.reduce((acc, curr) => {
+            const month = curr.Month;
+            const quarter = getQuarter(month); // You need to implement getQuarter function
+
+            // Accumulate sales for each quarter
+            acc[quarter] = (acc[quarter] || 0) + curr.Sales;
+
+            return acc;
+        }, {});
+
+        // Update the content of your HTML elements
+        q1.textContent = `${salesByMonth['Q1'] || 0}`;
+        q2.textContent = `${salesByMonth['Q2'] || 0}`;
+        q3.textContent = `${salesByMonth['Q3'] || 0}`;
+        q4.textContent = `${salesByMonth['Q4'] || 0}`;
+    };
+
+    // Call the updateQSales function with your data after the chart is drawn
+    updateQSales(salesChartData);
 };
 
 main();
+
+// Function to get the quarter from the month
+function getQuarter(month) {
+    const quarterMap = {
+        'Jan': 'Q1',
+        'Feb': 'Q1',
+        'Mar': 'Q1',
+        'Apr': 'Q2',
+        'May': 'Q2',
+        'Jun': 'Q2',
+        'Jul': 'Q3',
+        'Aug': 'Q3',
+        'Sep': 'Q3',
+        'Oct': 'Q4',
+        'Nov': 'Q4',
+        'Dec': 'Q4',
+    };
+    return quarterMap[month] || 'Unknown';
+}
+// Innitializes the default value of the chart
+changeDataSet();
+// End of analytics-container diagram
+
+// Code that handles copy upon click on Q1,Q2,Q3,Q4 total data box
+const statBoxes = Array.from(document.getElementsByClassName('stat-box'));
+statBoxes.forEach(statBox =>{
+    statBox.addEventListener('click', function() {
+
+        let copyText = this.querySelector('.copyOnClick').innerText;
+        const copyContent = async () => {
+            try{
+                await navigator.clipboard.writeText(copyText);
+
+                // Color change & text display
+                this.style.transition = 'background-color 0.2s ease-in-out, color 0.2s ease-in-out';
+                this.querySelector('.copyOnClick').innerText = 'Value copied!';
+                this.querySelector('.copyOnClick').style.fontSize = '18px';
+                this.style.backgroundColor = '#695BD1';
+                this.style.color = '#d4d4d4';
+                setTimeout(() => {
+                    this.style.backgroundColor = '';
+                    this.style.color = '';
+                    this.querySelector('.copyOnClick').innerText = copyText;
+                    this.querySelector('.copyOnClick').style.fontSize = '';
+                }, 500);
+            }
+            catch(err){
+                console.error('Copy to clipboard failed: ', err);
+            }
+        }
+        copyContent();
+    });
+})
+
+
